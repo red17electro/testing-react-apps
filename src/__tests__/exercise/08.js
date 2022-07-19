589290dca4ed456bd9510fcde1585bc3f9fdb89e
red17electro <red17electro@gmail.com>
2022-07-19 16:27:41 +0200

    upd: use fake component in tests;

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,7 +2,7 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {act, render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
@@ -28,6 +28,22 @@ test('exposes the count and increment/decrement functions', async () => {
   await userEvent.click(decrementButton)
   await userEvent.click(decrementButton)
   expect(counter).toHaveTextContent('-1')
+})
+
+test('test counter with step and initial count', async () => {
+  let result
+  function CounterWithProps(props) {
+    result = useCounter(props)
+    return null
+  }
+
+  render(<CounterWithProps step={2} initialCount={5} />)
+  expect(result.count).toBe(5)
+  act(() => result.increment())
+  expect(result.count).toBe(7)
+  act(() => result.decrement())
+  act(() => result.decrement())
+  expect(result.count).toBe(3)
 })
 
 /* eslint no-unused-vars:0 */
